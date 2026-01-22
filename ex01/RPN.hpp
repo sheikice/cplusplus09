c788c60a282d5b3029083920cb11ac78899376ca
skc <sheikouz@gmail.com>
2026-01-22 10:52:10 +0100

    [chore/ex01] - clean

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -11,21 +11,22 @@ class RPN
 		RPN();
 		~RPN();
 
-		static std::vector<std::string> getTokens(char*);
+		static std::vector<std::string> getTokens(const char*);
 		static bool isDigit(std::string&);
 		static bool isOperator(std::string&);
 		static void pushStack(std::string&, std::stack<long long>&);
 		static void operate(std::string&, std::stack<long long>&);
 
-		static long long addNbr(long long, long long);
 		static long long plus(long long, long long);
 		static long long minus(long long, long long);
 		static long long multiply(long long, long long);
 		static long long divide(long long, long long);
 		static long long modulo(long long, long long);
 
+		static const int OPERATIONS_CHOICE = 5;
+
 	public:
-		static void rpn(char*);
+		static void rpn(const char*);
 
 	class WrongEntryException: public std::exception
 	{
@@ -33,11 +34,6 @@ class RPN
 	};
 
 	class DivideByZeroException: public std::exception
-	{
-		virtual const char* what() const throw();
-	};
-
-	class BadStreamException: public std::exception
 	{
 		virtual const char* what() const throw();
 	};
